2c72bc8a11ca5e2033f67865d7b2593166b62ada
Mel Chang <mel510@users.noreply.github.com>
2017-11-21 14:43:40 -0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+     get '/users/sign_out' => 'devise/sessions#destroy'
+   end
+
   resources :pets
   resources :dogs
+
+  root 'appplication#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
